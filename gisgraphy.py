8fe18b6621b7348e3f8e226b085a163c741b5a98
Jeff McGee <JeffAMcGee@gmail.com>
2010-10-24 18:15:33 -0500

    created gisgraphy client

---
+++ gisgraphy.py
@@ -0,0 +1,33 @@
+from restkit import request, Resource, SimplePool
+from restkit.errors import RequestFailed
+import json
+from datetime import datetime
+from settings import settings
+
+class GisgraphyResource(Resource):
+
+    def __init__(self):
+        Resource.__init__(self,
+                settings.gisgraphy_url,
+                pool_instance=SimplePool(keepalive=2),
+                client_opts={'timeout':30},
+        )
+
+    def fulltext(self, q, headers=None, **kwargs):
+        try:
+            r = self.get('fulltext/fulltextsearch', headers, q=q, format="json", **kwargs)
+            return json.loads(r.body_string())["response"]["docs"]
+        except RequestFailed as failure:
+            print "%d while retieving url!"%failure.response.status_int
+            print failure.response.final_url
+            logging.error("%s while retrieving %s",
+                    failure.response.status,
+                    failure.response.final_url
+            )
+            raise
+
+if __name__ == '__main__':
+    res = GisgraphyResource()
+    f = res.fulltext('Austin TX')
+
+
