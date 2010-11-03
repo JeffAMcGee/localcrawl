b716c4bb41c2e2dba13a10009fd7475bab6f703b
Jeff McGee <JeffAMcGee@gmail.com>
2010-11-03 10:04:39 -0500

    fixed bug in view after removing local

--- _design/user/views/score/map.py
+++ _design/user/views/score/map.py
@@ -1,5 +1,5 @@
 def map(doc):
-    if doc['doc_type']=='User':
+    if doc['doc_type']=='User' and 'rfs' in doc and 'ats' in doc:
         from math import log
         avg = (doc['rfs'] + doc['ats']) /2.0
 	yield int(log(max(avg,1),2)),None
